202a2e5deabee2d021e41e93746b33dbcc7cd437
Nicolas Hurtado <nhd15c@gmail.com>
2020-08-10 18:54:12 -0400

    Configured NestJS environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { MessageService } from './services/message.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [MessageService],
 })
