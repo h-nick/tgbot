d6f7d52d5545027d411b2ad63e7fbb45590d9c60
Nicolas Hurtado <nhd15c@gmail.com>
2020-08-12 16:52:56 -0400

    Added AdminService to manage the bot and chats

---
+++ src/services/admin.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, HttpService } from '@nestjs/common';
+import { TypeOfMessage, Command } from '../data/message.data';
+import { SendMessageObj } from '../data/message.data';
+
+/*
+  Service includes actions to admin the BOT and the group chat.
+*/
+
+@Injectable()
+export class AdminService {
+  constructor(
+    private readonly httpService: HttpService,
+  ) { }
+
+  async kickUser(targetChat: number, targetUser: number): Promise<any> {
+    const objRes = {
+      chat_id: targetChat,
+      user_id: targetUser,
+    };
+
+    return await this.httpService.post('/kickChatMember', objRes).toPromise();
+  }
+
+  async censorUser(
+    targetChat: number,
+    targetUser: number,
+    seconds: number = 35,
+  ): Promise<any> {
+
+    const objRes = {
+      chat_id: targetChat,
+      user_id: targetUser,
+      until_date: Math.round(new Date(Date.now() + (seconds * 1000)).getTime() / 1000),
+      permissions: {
+        can_send_messages: false,
+        can_change_info: false,
+        can_pin_messages: false,
+      },
+    };
+
+    return await this.httpService.post('/restrictChatMember', objRes).toPromise();
+  }
+}
