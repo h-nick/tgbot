53d655bdd7ae55a86e6a4452daccc45d95425e75
Nicolas Hurtado <nhd15c@gmail.com>
2020-08-13 01:28:14 -0400

    Migrated command handlers to their own functions

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -10,73 +10,87 @@ import { AdminService } from 'src/services/admin.service';
 
 @Controller('/new-message')
 export class MessageController {
+  private message;
+
   constructor(
     private readonly messageService: MessageService,
     private readonly adminService: AdminService,
   ) { }
 
+  async handleTestCommand(): Promise<void> {
+    await this.messageService.sendMessage(this.message.chat.id, strings.TEST_STRING());
+  }
+
+  async handleCensorCommand(command: Command): Promise<void> {
+    let duration;
+    const uName = this.message.from.username || this.message.from.first_name;
+
+    // TODO: Dynamic times
+    if (command === Command.CENSOR) {
+      duration = 30;
+    } else if (command === Command.SUPERCENSOR) {
+      duration = 60;
+    } else {
+      duration = 300;
+    }
+
+    // Censor command only works when replyig to a message.
+    if (!this.message.reply_to_message) {
+      this.messageService.sendMessage(
+        this.message.chat.id,
+        strings.ERROR_NO_REPLY_ID_ON_CENSOR(uName),
+        this.message.message_id,
+      );
+
+      return;
+    }
+
+    const resStatus = await this.adminService.censorUser(
+      this.message.chat.id,
+      this.message.reply_to_message.from.id,
+      duration,
+    );
+
+    // If censorUser() is successful, it will return true.
+    if (!resStatus) {
+      this.messageService.sendMessage(
+        this.message.chat.id,
+        strings.CENSOR_ERROR(),
+        this.message.from.id,
+      );
+    } else {
+      this.messageService.sendMessage(
+        this.message.chat.id,
+        strings.CENSOR_MESSAGE(uName, duration),
+        this.message.reply_to_message.message_id,
+      );
+    }
+  }
+
   @Post()
   async handleMessage(@Req() req, @Res() res): Promise<void> {
     try {
-      const message = req.body.message;
+      this.message = req.body.message;
 
       // Only handle text messages. Stickers, videos, photos, etc. are ignored.
-      if (message.text) {
-        const typeOfMessage = this.messageService.getTypeOfMessage(message.text);
+      if (this.message.text) {
+        const typeOfMessage = this.messageService.getTypeOfMessage(this.message.text);
 
         // So far only commands will be handled. It can be easily escalated to handle
         // normal messages too.
         if (typeOfMessage === TypeOfMessage.COMMAND) {
-          const command = this.messageService.getCommand(message.text);
+          const command = this.messageService.getCommand(this.message.text);
 
           // No need for a default case. It will return in the end.
           switch (command) {
             case Command.TEST:
-              await this.messageService.sendMessage(message.chat.id, strings.TEST_STRING());
+              this.handleTestCommand();
               break;
 
             case Command.CENSOR:
             case Command.SUPERCENSOR:
             case Command.MEGACENSOR:
-              let msg;
-              let duration;
-              const uName = message.from.username || message.from.first_name;
-
-              // TODO: Dynamic times
-              if (command === Command.CENSOR) {
-                duration = 30;
-              } else if (command === Command.SUPERCENSOR) {
-                duration = 60;
-              } else {
-                duration = 300;
-              }
-
-              // Censor command only works when replyig to a message.
-              if (!message.reply_to_message) {
-                msg = strings.ERROR_NO_REPLY_ID_ON_CENSOR(uName);
-                this.messageService.sendMessage(message.chat.id, msg, message.message_id);
-                break;
-              }
-
-              const resStatus = await this.adminService.censorUser(
-                message.chat.id,
-                message.reply_to_message.from.id,
-                duration,
-              );
-
-              // If censorUser() is successful, it will return true.
-              if (!resStatus) {
-                msg = strings.CENSOR_ERROR();
-                this.messageService.sendMessage(message.chat.id, msg, message.from.id);
-              } else {
-                msg = strings.CENSOR_MESSAGE(uName, duration);
-
-                await this.messageService.sendMessage(
-                  message.chat.id,
-                  msg,
-                  message.reply_to_message.message_id,
-                );
-              }
+              this.handleCensorCommand(command);
               break;
           }
         }
@@ -99,7 +113,10 @@ export class MessageController {
 
       console.log(errorMsg);
 
-      await this.messageService.sendMessage(Number(process.env.DEFAULT_GROUP_ID), errorMsg);
+      await this.messageService.sendMessage(
+        Number(process.env.DEFAULT_GROUP_ID),
+        errorMsg,
+      );
     }
 
     return res.status(200).send();
